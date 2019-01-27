3812e0b412eb06506f813c289566afd46e4fcec1
David Koňařík <dvd.kon@gmail.com>
2019-01-27 12:38:23 +0100

    Change permission primary key to be its name

--- create_schema.sql
+++ create_schema.sql
@@ -3,15 +3,17 @@ CREATE TABLE IF NOT EXISTS users_current (
     username text NOT NULL
 );
 SELECT version_table('users');
+-- XXX: Add permissions when copying this sample
+SELECT dascore_setup_table('users', '0=0');
 
 CREATE TABLE IF NOT EXISTS permissions (
-    id serial PRIMARY KEY,
-    name text UNIQUE NOT NULL
+    name text PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS user_permissions_current (
     id_user integer REFERENCES users_current NOT NULL,
-    id_permission integer REFERENCES permissions NOT NULL,
-    PRIMARY KEY (id_user, id_permission)
+    permission text REFERENCES permissions NOT NULL,
+    PRIMARY KEY (id_user, permission)
 );
 SELECT version_table('user_permissions');
+SELECT dascore_setup_table('user_permissions', '0=0');
